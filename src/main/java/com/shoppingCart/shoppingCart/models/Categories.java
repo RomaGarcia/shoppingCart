de6e85e9915f51e8087c80404225a434146a215c
ramiro.a.rodriguez <ramirorodriguezgiemenez@gmail.com>
2022-09-01 11:36:59 -0300

    entidades y dependencia rest

--- src/main/java/com/shoppingCart/shoppingCart/models/Categories.java
+++ src/main/java/com/shoppingCart/shoppingCart/models/Categories.java
@@ -1,4 +1,56 @@
 package com.shoppingCart.shoppingCart.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
 public class Categories {
+    @Id
+    @GeneratedValue (strategy= GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native",strategy = "native")
+    private Long id;
+    private String name;
+    @OneToMany ( fetch = FetchType.EAGER)
+    private Set<Product> product=new HashSet<>();
+    public Categories() {
+    }
+
+    public Categories(String name) {
+        this.name = name;
+    }
+
+    public Set<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(Set<Product> product) {
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Categories{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/shoppingCart/shoppingCart/models/ProductLoad.java
+++ src/main/java/com/shoppingCart/shoppingCart/models/ProductLoad.java
@@ -1,4 +1,56 @@
 package com.shoppingCart.shoppingCart.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
 public class ProductLoad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name="native", strategy = "native")
+    private Long id;
+    @ManyToOne
+    private Product product;
+    private Integer amount;
+    public ProductLoad() {
+    }
+
+    public ProductLoad(Product product, Integer amount) {
+        this.product = product;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductLoad{" +
+                "id=" + id +
+                ", product=" + product +
+                ", amount=" + amount +
+                '}';
+    }
 }
